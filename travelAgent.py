0c1faf9ce28b7e345ab294106c0f643a97d69568
Tamicktom <rickpokemon38@gmail.com>
2024-07-12 08:04:17 -0300

    update script

--- travelAgent.py
+++ travelAgent.py
@@ -1,27 +1,24 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_community.agent_toolkits.load_tools import load_tools
-from langchain.agents import initialize_agent
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
 tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
 
-# print(tools[0].name, tools[0].description)
-# print(tools[1].name, tools[1].description)
+prompt = hub.pull("hwchase17/react")
 
-agent = initialize_agent(
-  tools, 
-  llm=llm, 
-  agent='zero-shot-react-description',
-  verbose=True
-)
+agent = create_react_agent(llm, tools, prompt)
 
-print(agent.agent.llm_chain.prompt.template)
+agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
 
 query = """
 Vou viajar para Londres em agosto de 2024.
 Quero que faça um roteiro de viagem para mim com os eventos que irão ocorrer na data da viagem e com o preço de passagem de São Paulo para Londres.
 """
 
-agent.run(query)
+agent_executor.invoke({
+  "input": query,
+})
