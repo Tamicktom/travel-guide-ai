d433ee052b3bb551bad1655016035bd2bf3dbbfb
Tamicktom <rickpokemon38@gmail.com>
2024-07-14 13:42:12 -0300

    it works now... fuck you python!

--- travelAgent.py
+++ travelAgent.py
@@ -10,83 +10,90 @@ import bs4
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
+
 from langchain_core.runnables import RunnableSequence
+
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
 query = """
-Vou viajar para Londres em Agosto de 2024.
-Quero que faça um plano de viagem para mim com eventos que irão ocorrer nada data da minha viagem.
+Vou viajar para Londres em agosto de 2024.
+Quero que faça para um roteiro de viagem para mim com eventos que irão ocorrer na data da viagem e com o preço de passagem de São Paulo para Londres.
 """
 
-def researchAgent(query:str, llm: ChatOpenAI):
-  tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
-  prompt = hub.pull("hwchase17/react")
-  agent = create_react_agent(llm, tools, prompt)
-  agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose=True)
-  webContext = agent_executor.invoke({
-    "input":query
-  })
-  return webContext
+
+def researchAgent(query, llm):
+    tools = load_tools(["ddg-search", "wikipedia"], llm=llm)
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(
+        agent=agent, tools=tools, prompt=prompt, verbose=True
+    )
+    webContext = agent_executor.invoke({"input": query})
+    return webContext["output"]
+
+
+# print(researchAgent(query, llm))
+
 
 def loadData():
-  loader = WebBaseLoader(
-    web_paths=("https://www.dicasdeviagem.com/inglaterra/"),
-    bs_kwargs=dict(
-      parse_onlu=bs4.SoupStrainer(
-        class_=("postcontentwrap", "pagetitleloading")
-      )
-    ),
-  )
-  docs = loader.load()
-  text_spliter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, 
-    chunk_overlap=200
-  )
-  splits = text_spliter.split_documents(docs)
-  vector_store = Chroma.from_documents(
-    documents=splits, 
-    embedding=OpenAIEmbeddings()
-  )
-  retriever = vector_store.as_retriever()
-  return retriever
+    loader = WebBaseLoader(
+        web_paths=("https://www.dicasdeviagem.com/inglaterra/",),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=(
+                    "postcontentwrap",
+                    "pagetitleloading background-imaged loading-dark",
+                )
+            )
+        ),
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever()
+    return retriever
 
-def getRelevantDocs(query:str):
-  retriever = loadData()
-  relevant_documents = retriever.invoke(query)
-  return relevant_documents
 
-def supervisorAgent(query:str, llm: ChatOpenAI, webContext, relevant_documents):
-  prompt_template = """
-    Você é um gerente de uma agência de viagens. Sua resposta final deverá ser um roteiro de viagem completo e detalhado.
-    Utilize o contexto de eventos e preços de passagens, o input do usuário e também os documentos relevantes para elaborar o roteiro.
-    
-    Contexto: {webContext}
-    Documentos Relevantes: {relevant_documents}
-    Usuário: {query}
-    Assistete: 
+def getRelevantDocs(query):
+    retriever = loadData()
+    relevant_documents = retriever.invoke(query)
+    print(relevant_documents)
+    return relevant_documents
+
+
+def supervisorAgent(query, llm, webContext, relevant_documents):
+    prompt_template = """
+  Você é um gerente de uma agência de viagens. Sua resposta final deverá ser um roteiro de viagem completo e detalhado. 
+  Utilize o contexto de eventos e preços de passagens, o input do usuário e também os documentos relevantes para elaborar o roteiro.
+  Contexto: {webContext}
+  Documento relevante: {relevant_documents}
+  Usuário: {query}
+  Assistente:
   """
-  
-  prompt = PromptTemplate(
-    input_variables=["webContext", "relevant_documents", "query"],
-    template=prompt_template
-  )
-  
-  sequence = RunnableSequence(prompt | llm)
-  
-  response = sequence.invoke({
-    "webContext":webContext,
-    "relevant_documents":relevant_documents,
-    "query":query
-  })
-  
-  return response
+
+    prompt = PromptTemplate(
+        input_variables=["webContext", "relevant_documents", "query"],
+        template=prompt_template,
+    )
+
+    sequence = RunnableSequence(prompt | llm)
+    response = sequence.invoke(
+        {
+            "webContext": webContext,
+            "relevant_documents": relevant_documents,
+            "query": query,
+        }
+    )
+    return response
+
 
 def getResponse(query, llm):
-  webContext = researchAgent(query, llm)
-  relevant_documents = getRelevantDocs(query)
-  
-  response = supervisorAgent(query, llm, webContext, relevant_documents)
-  return response
+    webContext = researchAgent(query, llm)
+    relevant_documents = getRelevantDocs(query)
+    response = supervisorAgent(query, llm, webContext, relevant_documents)
+    return response
 
-print(getResponse(query, llm))
+
+print(getResponse(query, llm).content)
